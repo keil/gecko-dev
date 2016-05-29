5693f0f29449d1248452835051e86a83b8492611
Omer Farooq <rfvt12345@gmail.com>
2016-05-29 13:18:55 -0700

    continuing work on making map realm-aware

--- js/src/builtin/MapObject.cpp
+++ js/src/builtin/MapObject.cpp
@@ -727,35 +727,38 @@ MapObject::set(JSContext* cx, unsigned argc, Value* vp)
         RootedValue current_map(cx,args.thisv());
 
         //A dummy object if the map is not part of realm
-        RootedObject emptyObject(cx);
+        //RootedString name_str(cx, JS_NewStringCopyZ(cx, "Dave"));
+        RootedObject emptyObject(cx, JS_GetGlobalForObject(cx, &args.callee()));
+        RootedValue emptyVal(cx);
+        JS_SetReservedSlot(emptyObject,0,emptyVal);
+        
+        
+        bool someVal = false;
         
         //Checking if realm object exists in the reserved slot of the map(To check If map is part of a realm)
-        bool someVal = false;
-
         if(!JS_GetReservedSlot(&current_map.toObject(),0).isNullOrUndefined())
         {
             someVal = true;
         }
               
-        //RootedValue* realm2 = someVal ? RootedValue(cx,JS_GetReservedSlot(&current_map.toObject(),0)) : RootedValue(cx,JS_GetReservedSlot(&current_map.toObject(),0))
-        RootedValue realm(cx,JS_GetReservedSlot(someVal ? &current_map.toObject():emptyObject,0));    
-        RootedObject ozz(cx,&realm.toObject());
+        RootedValue realm(cx,JS_GetReservedSlot(someVal ? &current_map.toObject():emptyObject,0)); 
+        RootedObject ozz(cx,someVal ? &realm.toObject():emptyObject);
     
         
-            
 
-        //Getting the realm object of the target if any
-        JSObject* obj_temp = &args[0].toObject();
-        const JS::Value* set_object_realm = &obj_temp->as<js::ProxyObject>().extra(2);
-        RootedObject ojj(cx,&set_object_realm->toObject());
-
-        if(ozz==ojj)
-            RootedValue val(cx);
+        
 
         
         if(IsTransparentProxy(&args[0].toObject()))
         {
-            args[0].setObject(*GetIdentityObject(&args[0].toObject()));
+            //Getting the realm object of the target if any
+            JSObject* obj_temp = &args[0].toObject();
+            const JS::Value* set_object_realm = &obj_temp->as<js::ProxyObject>().extra(2);
+            RootedObject ojj(cx,&set_object_realm->toObject());
+            if(ozz==ojj)
+                args[0].setObject(args[0].toObject());
+            else
+                args[0].setObject(*GetIdentityObject(&args[0].toObject()));
         }
     }
     return CallNonGenericMethod<MapObject::is, MapObject::set_impl>(cx, args);
